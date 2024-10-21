30c9a271efb8f419f542b6085c7dec281fc2cbac
SAGARPORWAL-89 <sagarporwal8370@gmail.com>
2024-10-21 17:02:15 +0530

    'adeed'

---
+++ app.js
@@ -0,0 +1,56 @@
+// Import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config()
+// Initialize the app
+const app = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/userDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+// new update
+// Create a Mongoose Schema now date 
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+// Create a model from the schema
+const User = mongoose.model('User', userSchema);
+
+// Set up middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    // Extract the submitted form data
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Create a new user and save to the database
+    const newUser = new User({
+        username: username,
+        password: password
+    });
+
+    newUser.save()
+    res.send("<b>Incorrect Password</b>")
+});
+
+// Start the server
+const PORT = 4141;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
